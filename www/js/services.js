d2935c35cea3983dc2cde1c705edb933fb1fef65
ziax <ziaxdk@gmail.com>
2014-09-30 18:54:47 +0200

    grunt build

--- www/js/services.js
+++ www/js/services.js
@@ -71,6 +71,9 @@ angular.module('ziaxgazapp.services', ['ziaxgazapp.constants'])
 
 .service('Hardware', [function() {
   this.vibrate = function(timeMs) {
+    alert(navigator);
+    alert(navigator.notification);
+    alert(navigator.notification.vibrate);
     if (navigator && navigator.notification && navigator.notification.vibrate) {
       navigator.notification.vibrate(200);
     } else {
